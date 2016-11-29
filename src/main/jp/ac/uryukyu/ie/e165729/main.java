de134b196521beffdd0cb1d69ecad6a73240f565
Takahiro Ikki <e165713@ie.u-ryukyu.ac.jp>
2016-11-29 11:26:14 +0900

    演習３

--- src/main/jp/ac/uryukyu/ie/e165729/main.java
+++ src/main/jp/ac/uryukyu/ie/e165729/main.java
@@ -11,11 +11,32 @@ public class main {
     public static void main(String[] args) {
         String filename = "./src/main/jp/ac/uryukyu/ie/e165729/map.txt";
         Scanner scanner;
+        int[] fast_place = new int[2];
+        int[] size_of_map = new int[2];
+
         try {
             scanner = new Scanner(new File(filename));
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                System.out.println(line);
+            for(int i = 0;scanner.hasNextLine();i++) {
+                if (i == 0) {
+                    for(int k = 0; k <= 1; k++) {
+                        String line1 = scanner.next();
+                        int place = Integer.parseInt(line1);
+                        fast_place[k] = place;
+                    }
+                    // 座標の表示
+                    System.out.printf("座標 x:%d y:%d\n",fast_place[0],fast_place[1]);
+                } else if (i == 1) {
+                    for(int k = 0; k <= 1; k++) {
+                        String line2 = scanner.next();
+                        int size = Integer.parseInt(line2);
+                        size_of_map[k] = size;
+                    }
+                    // 大きさの表示
+                    System.out.printf("迷路の大きさ：横%d　縦%d\n",size_of_map[0],size_of_map[1]);
+                } else {
+                    String line = scanner.nextLine();
+                    System.out.println(line);
+                }
             }
             scanner.close();
         }catch (FileNotFoundException e) {
