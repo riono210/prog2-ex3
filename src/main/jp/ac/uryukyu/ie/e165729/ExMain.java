4fbff9ec837517c5fe64bed89e0900d15c3a49a9
Takahiro Ikki <e165713@ie.u-ryukyu.ac.jp>
2016-12-13 11:25:46 +0900

    演習3_3

---
+++ src/main/jp/ac/uryukyu/ie/e165729/ExMain.java
@@ -0,0 +1,22 @@
+package jp.ac.uryukyu.ie.e165729;
+
+/**
+ * Created by ichikitakahiro on 2016/12/13.
+ */
+public class ExMain {
+    public static void main(String[] args){
+        Maze maze = new Maze;
+        Player player = new Player;
+
+
+        maze.map();
+        player.leftmove();
+        player.map();
+        player.downmove();
+        player.map();
+        player.rightmove();
+        player.map();
+        
+    }
+
+}
